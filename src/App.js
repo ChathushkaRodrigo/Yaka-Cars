bf3c61c1aed21901bf16b2a66224edbae85347a6
ChathushkaRodrigo <chathushkaseniyarodrigo@protonmail.com>
2021-12-26 14:04:48 +0530

    Styled Header and made it more responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-// import Header from './components/Header';
+import Header from './components/Header';
 import Home from './components/Home';
 function App() {
   return (
     <div className="App">
-{/* 
-<Header/> */}
+
+<Header/>
     <Home/>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,68 @@
 import React from 'react'
-
+import styled from 'styled-components';
+import MenuIcon from '@material-ui/icons/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
 function Header() {
     return (
-        <div>
-            Header
-
-        </div>
+        <Container>
         
+        <a>
+            <img src ="/images/logo.svg" alt=""/>
+        </a>
+        <Menu>
+           <a href="#">Model S</a>            
+            <a href="#">Model 3</a> 
+            <a href="#">Model X</a>
+            <a href="#">Model Y</a>
+        </Menu>
+        <RightMenu>
+        <a href="#">Shop</a> 
+        <a href="#">Tesla Account</a> 
+        <CustomMenu/>
+        </RightMenu>
+
+       
+        </Container>
     )
 }
 
 export default Header
+const Container =styled.div`
+min-height:60px;
+position:fixed;
+display:flex;
+align-items:center;
+padding:0 20px;
+justify-content:space-between;
+top:0;
+left:0;
+right:0;
+`
+const Menu =styled.div`
+display:flex;
+align-items:center;
+flex:1;
+justify-content:center;
+a{
+    font-weight:600;
+    text-transform:uppercase;
+    padding:0 10px;
+    flex-wrap:no-wrap;
+}
+@media(max-width:768px){
+    display:none;
+}
+`
+const RightMenu =styled.div`
+display:flex;
+align-items:center;
+
+a{
+    font-weight:600;
+    text-transform:uppercase;
+    margin-right:10px;
+}
+`
+const CustomMenu=styled(MenuIcon)`
+cursor:pointer;
+`
